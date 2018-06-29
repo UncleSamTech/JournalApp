c3f09ad86ec5f58b9c26c5020c1fcb7b09b9c284
Samuel Joel <31622538+UncleSamTech@users.noreply.github.com>
2018-06-29 00:22:40 -0700

    RegistrationScreenActivity.java
    Modified this class by adding methods for enabling registration of new users in the database

--- RegistrationScreenActivity.java
+++ RegistrationScreenActivity.java
@@ -2,6 +2,9 @@ package com.journalapp.samuel.journalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class RegistrationScreenActivity extends AppCompatActivity {
 
@@ -11,4 +14,27 @@ public class RegistrationScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration_screen);
 
     }
+    
+    
+      private EditText getEditText(int id){
+        EditText editText = findViewById(id);
+        return editText;
+    }
+
+    private Button getButton(int id){
+        Button button = findViewById(id);
+        return button;
+    }
+
+    private class Resources{
+        String userName = getEditText(R.id.edit_text_email_reg_id).getText().toString().trim();
+        String passWord = getEditText(R.id.edit_text_pass_reg_id).getText().toString().trim();
+
+
+
+        public Resources(){}
+
+    }
+
+
 }
